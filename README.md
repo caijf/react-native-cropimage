8b9d17c2b932cb85d12b04f1075d2242582ea2b0
caijf <369756941@qq.com>
2016-06-21 23:32:44 +0800

    处理雪碧图

--- README.md
+++ README.md
@@ -1,1 +1,44 @@
 # react-native-cropimage
+
+处理雪碧图
+
+### Getting started
+
+安装 `react-native-cropimage`
+
+	npm install react-native-cropimage --save
+
+使用
+
+	// ...
+	
+	import CropImage from 'react-native-cropimage';
+	
+	class IconHome extends Component{
+		return (
+			<CropImage
+				source={require('./icon-business.png')}
+				crop={{
+					top: 0,
+					left: 0,
+					width: 20,
+					height: 20
+				}}
+				width={60}
+				height={120}							
+			/>
+		)
+	}
+
+
+### API
+- **source** - (Object) - 图片源，必须
+- **width** - (Number) - 图片宽度，必须
+- **height** - (Number) - 图片高度，必须
+- **crop** - (Object):
+	- **top** - (Number) - 裁剪起点 y轴，以左上角为起点，默认0
+	- **left** - (Number) - 裁剪起点 x轴，以左上角为起点，默认0
+	- **width** - (Number) - 裁剪宽度，默认图片宽度
+	- **height** - (Number) - 裁剪高度，默认图片高度
+- **onPress** - (Function) - 点击图片回调（一般用不到，在用到的组件里写事件）
+- **style** - (Object, Array) - 图片外层样式，一般用不到
---
+++ index.js
@@ -0,0 +1,53 @@
+import React, {Component, PropTypes} from 'react';
+import {
+    Image,
+    View,
+    TouchableWithoutFeedback
+} from 'react-native';
+
+export default class CropImage extends Component{
+    render() {
+        let source = this.props.source;
+        let crop = this.props.crop || source.crop;
+
+        let {source, crop, width, height, children, onPress} = this.props;
+
+        crop = crop || source.crop;
+
+        crop.top = crop.top || 0;
+        crop.left = crop.left || 0;
+        crop.width = crop.width || width;
+        crop.height = crop.height || height;
+
+        return (
+                <View style={[{
+                    overflow: 'hidden',
+                    height: crop.height,
+                    width: crop.width,
+                    backgroundColor: 'transparent'
+                }]}>
+                    <Image onPress={onPress} style={{
+                        marginTop: crop.top * -1,
+                        marginLeft: crop.left * -1,
+                        width: width,
+                        height: height
+                    }} source={source} resizeMode='contain'>
+                        {children}
+                    </Image>
+                </View>
+        );
+    }
+
+    static propTypes = {
+        source: PropTypes.object.isRequired,
+        crop: PropTypes.object,
+        width: PropTypes.number.isRequired,
+        height: PropTypes.number.isRequired,
+        onPress: PropTypes.func
+    };
+
+    static propTypes = {
+        onPress: ()=>{}
+    };
+}
+
